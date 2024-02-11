53078849f631f02eacc0cea8f4b4ca1cef0572a8
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2024-02-11 16:38:34 +0100

    Setup deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/accordion-fingering-practice-midi/", // https://vitejs.dev/guide/static-deploy#github-pages
   plugins: [react()],
 });

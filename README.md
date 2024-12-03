dd005293dabfeb2364a88fb2c2eced8a98faa5ca
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2024-12-03 21:34:14 +0100

    Fresh start

--- README.md
+++
@@ -1,7 +0,0 @@
-# (WIP) accordion-fingering-practice-midi
-
-Randomly generated accorion fingering practice, supports MIDI input.
-
-Mainly for trying out my Roland FR-1XB.
-
-Bundler setup was created using `npm create vite@5.1.0 . -- --template react-ts`.
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/accordion-fingering-practice-midi/", // https://vitejs.dev/guide/static-deploy#github-pages
-  plugins: [react()],
-});

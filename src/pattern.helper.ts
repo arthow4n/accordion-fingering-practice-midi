6bb3c08ed3eaac32a87a71429f676dd8b49b4066
Sheng-Han (Aysh) Su <13860646+arthow4n@users.noreply.github.com>
2025-04-19 19:02:13 +0200

    Add all available right hand notes on FR-1XB

--- src/pattern.helper.ts
+++ src/pattern.helper.ts
@@ -47,6 +47,17 @@ export const rightHandNotesRange: Note[] = [
   "F#5",
   "G5",
   "G#5",
+  "A5",
+  "A#5",
+  "B5",
+  "C6",
+  "C#6",
+  "D6",
+  "D#6",
+  "E6",
+  "F6",
+  "F#6",
+  "G6",
 ].map(noteStringToNote);
 
 export const bassKeyRange: BassBaseRoot[] = [
